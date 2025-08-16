e5c977b184286492516f56b52c2c15128ba11055
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-08-16 14:28:49 +0400

    add_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["breneoapi-vftvf.ondigitalocean.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
