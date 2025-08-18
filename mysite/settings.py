ef5fa61846e7217e34e643840349270da49c1284
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-08-19 00:18:09 +0400

    change host

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,8 +8,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ['*']
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ['breneo-hm2jx.ondigitalocean.app']
 
 DEBUG=False
 
