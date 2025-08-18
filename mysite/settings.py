144c5a4e981dd0d893b3ab6d1e8893b5edea0eaf
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-08-19 00:01:17 +0400

    change host_adress

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,8 +8,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
-DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ['breneoapi-vftvf.ondigitalocean.app']
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ['breneo-4wfh3.ondigitalocean.app']
+
+DEBUG=False
 
 INSTALLED_APPS = [
     'django.contrib.admin',
