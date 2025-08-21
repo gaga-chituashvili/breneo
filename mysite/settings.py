f8e3ce1f6ba3ad82612a0fdcbd90718b55c56506
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-08-21 16:15:42 +0400

    add_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["breneo.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
