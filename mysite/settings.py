0c452c987fa2e9bd3fac5644a290006ddd8d2ef5
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-10-24 19:57:32 +0400

    feat:add cloudinary

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,20 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     "corsheaders",
     "app",
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware", 
